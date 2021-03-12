83c8f3a208dbe63a3df0a14e29d694c2b60023a4
SergioEstevao11 <smestevao11@gmail.com>
2021-03-12 10:36:57 +0000

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+int main(int argc, char* argv[], char* envp[]){
+
+    printf("Hello World :3\n");
+    return 0;
+}
